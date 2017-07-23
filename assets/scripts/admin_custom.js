94f7e938368499f0d5c327ee6eb8f99dc382bbae
praveenak <ak@bluelagoontechnologies.com>
2017-07-23 11:02:41 +0530

    add custom js and js color --praveen ak

---
+++ assets/scripts/admin_custom.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+	$(".switch").change(function(){
+		var val=$(this).attr("data-value");
+		if(val==1)
+		{
+			$(this).attr("data-value","0");
+			$("#site_status").attr("value","0");
+			$(".site_status").removeClass("success");
+			$(".site_status").addClass("error");
+			$(".site_status").html("Site is on Maintanence..");
+		}
+		else
+		{
+			$(this).attr("data-value","1");
+			$("#site_status").attr("value","1");
+			$(".site_status").addClass("success");
+			$(".site_status").removeClass("error");
+			$(".site_status").html("Site is working :) ");
+		}
+	})
+
+	$("#upload_logo").change(function() {
+		$("#ul").submit();
+    });
+	$("#upload_icon").change(function() {
+		$("#ui").submit();
+    });
+
+	$("#update_color").click(function(){
+		var url="set_sitecolor";
+	    var color=$("#site_color").val();
+	    $.ajax(
+	         {
+	            type:"POST",
+	            cache:false,
+	            url: url, 
+	            data : {
+	               "_token": $('meta[name="_token"]').attr('content'),
+	               "color":color
+	            },
+	            success: function(result){
+	                  window.location.href = "settings";
+	            }
+	        });
+	})
+
+})
