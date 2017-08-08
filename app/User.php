58a287969368b89454119790a97352f7fb33c0d1
praveenak <ak@bluelagoontechnologies.com>
2017-08-08 23:09:16 +0530

    changes in user -praveen ak

--- app/User.php
+++ app/User.php
@@ -14,7 +14,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-   protected $fillable = ['name','email','password','profile','phone','city','countrycode','role'];
+   protected $fillable = ['name','email','password','profile','phone','city','countrycode','role','lat','lang'];
 
     /**
      * The attributes that should be hidden for arrays.
