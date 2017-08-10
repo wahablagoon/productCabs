b7f516490ed430ae063deae184c7765a384ca6df
praveenak <ak@bluelagoontechnologies.com>
2017-08-10 07:46:26 +0530

    add generate file function -Praveen ak

--- app/Helper/common_helper.php
+++ app/Helper/common_helper.php
@@ -117,3 +117,13 @@ function generatePIN($digits = 4){
     }
     return $pin;
 }
+function random_string($length) {
+    $key = '';
+    $keys = array_merge(range(0, 9), range('a', 'z'));
+
+    for ($i = 0; $i < $length; $i++) {
+        $key .= $keys[array_rand($keys)];
+    }
+
+    return $key;
+}
