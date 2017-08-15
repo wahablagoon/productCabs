f0d778421e1b2a22b7bebd9102127fdb33debb08
praveenak <ak@bluelagoontechnologies.com>
2017-08-15 20:45:05 +0530

    changes --praveen ak

--- assets/scripts/admin_custom.js
+++ assets/scripts/admin_custom.js
@@ -1,4 +1,214 @@
 $(document).ready(function(){
+$('#expired').datepicker({
+    format: 'dd/mm/yyyy',
+    startDate: '-3d',
+    autoclose: true
+});
+
+$("#ptype_per").click(function(){
+$(".price_tag").html("%");
+})
+$("#ptype_price").click(function(){
+$(".price_tag").html("$");
+})
+
+
+$("#close_promo").click(function(){
+  var frm = document.getElementsByName('addpromo')[0];
+  frm.reset();
+  $('input[type=checkbox]').prop('checked', false);
+})
+$(".add_promo").click(function(){
+  var frm = document.getElementsByName('addpromo')[0];
+  frm.reset();
+  $('input[type=checkbox]').prop('checked', false);
+  $("#edity").val("no");
+})
+
+$(".view_edit_promo").click(function(){
+  $("#edity").val("yes");
+  var id=$(this).attr("data-id");
+  $("#promo_id").val(id);
+  var pid="#promo_"+id;
+  $("#price").val($(pid).attr("data-amount"));
+    $("#code").val($(pid).attr("data-code"));
+  $("#expired").val($(pid).attr("data-expired"));
+  if($(pid).attr("data-status")=="1")
+  {
+  $('input:checkbox[name="status"]').prop('checked', true);  
+  } 
+  else
+  {
+    $('input:checkbox[name="status"]').prop('checked', false);
+  }
+  
+ 
+})
+
+$("#add_promo").click(function(){
+
+  var price=$("#price").val();
+  var edit=$("#edity").val();
+  var pid=$("#promo_id").val();
+  var code=$("#code").val();
+  var expired=$("#expired").val();
+  var status=$(".switch").attr("data-value");
+  var type=$('input[name="ptype"]:checked').val();
+  if(status === undefined)
+  {
+    status=0;
+  }
+
+  if(code=="" || code==null)
+  {
+    Materialize.toast('Please enter promo !', 4000)
+  }
+  else if(expired=="")
+  {
+    Materialize.toast('Please select Expired date !', 4000)
+  }
+  else if(price=="" || parseInt(price)<=0)
+  {
+    Materialize.toast('Please enter price !', 4000)  
+  }
+  else
+  {
+    $('#add_promo').addClass('disabled');
+    $.ajax({
+      type: "POST",
+      dataType: "json",
+      url: APP_URL+"/addPromo",
+      data: {id:pid,edit:edit,code: code,expired: expired,price: price,status:status,type:type},
+      success: function(output){
+        if(output[0].stats=="fail")
+        {
+          $('#add_promo').removeClass('disabled');
+          Materialize.toast(output[0].errors, 4000)
+        }
+        else
+        {
+          var frm = document.getElementsByName('addpromo')[0];
+          frm.reset();
+          location.reload(); 
+        }
+      }
+    });
+  }
+  
+})
+
+
+if(window.location.href.indexOf("surge") > -1) {
+
+var input_start_peak = $('#start_peak').clockpicker({
+  placement: 'bottom',
+  align: 'left',
+  autoclose: true,
+  twelvehour:true,
+  'default': 'now'
+});
+
+var input_end_peak = $('#end_peak').clockpicker({
+  placement: 'bottom',
+  align: 'left',
+  autoclose: true,
+  twelvehour:true,
+  'default': 'now'
+});
+
+$("#close_peak").click(function(){
+  var frm = document.getElementsByName('addpeak')[0];
+  frm.reset();
+  $('input[type=checkbox]').prop('checked', false);
+})
+$(".add_peak").click(function(){
+  var frm = document.getElementsByName('addpeak')[0];
+  frm.reset();
+  $('input[type=checkbox]').prop('checked', false);
+$("#edity").val("no");
+})
+
+$(".view_edit_peak").click(function(){
+$("#edity").val("yes");
+  var id=$(this).attr("data-id");
+  $("#peak_id").val(id);
+  var pid="#peaks_"+id;
+  $("#price").val($(pid).attr("data-amount"));
+  $("#start_peak").val($(pid).attr("data-start"));
+  $("#end_peak").val($(pid).attr("data-end"));
+$("#category").val($(pid).attr("data-category"));
+$('#category').material_select();
+  if(!$('input:radio[name="ptype"][value="' + $(pid).attr("data-type") + '"]').is(':checked'))
+    $('input:radio[name="ptype"][value="' + $(pid).attr("data-type") + '"]').prop('checked', true);
+
+  var res = $(pid).attr("data-days").split(","); 
+  $('input[type=checkbox]').prop('checked', false);
+  $(res).each(function(key, value){
+    if(!$('input:checkbox[name="days[]"][value="' + value + '"]').is(':checked'))
+      $('input:checkbox[name="days[]"][value="' + value + '"]').prop('checked', true);
+  })
+ 
+})
+
+$("#add_peak").click(function(){
+  var price=$("#price").val();
+  var edit=$("#edity").val();
+  var pid=$("#peak_id").val();
+  var start_peak=$("#start_peak").val();
+  var end_peak=$("#end_peak").val();
+  var category=$("#category").val();
+  var daystotal=$("#add_peak_popup").find('input[name="days[]"]:checked').length;
+  var days = $("#add_peak_popup").find('input[name="days[]"]:checked').map(function() {return this.value;}).get().join(',');
+  var type=$('input[name="ptype"]:checked').val();
+ 
+  if(daystotal=="0")
+  {
+    Materialize.toast('Please select any  one days !', 4000)
+  }
+  else if(category=="" || category==null)
+  {
+    Materialize.toast('Please select category !', 4000)
+  }
+  else if(start_peak=="00:00")
+  {
+    Materialize.toast('Please select Start time !', 4000)
+  }
+  else if(end_peak=="00:00")
+  {
+    Materialize.toast('Please select End time !', 4000)
+  }
+  else if(price=="" || parseInt(price)<=0)
+  {
+    Materialize.toast('Please enter price !', 4000)  
+  }
+  else
+  {
+    $('#add_peak').addClass('disabled');
+    $.ajax({
+      type: "POST",
+      dataType: "json",
+      url: APP_URL+"/addPeak",
+      data: {id:pid,edit:edit,category: category,start_peak: start_peak,end_peak: end_peak,price: price,days:days,type:type},
+      success: function(output){
+        if(output[0].stats=="fail")
+        {
+          $('#add_peak').removeClass('disabled');
+          Materialize.toast(output[0].errors, 4000)
+        }
+        else
+        {
+          var frm = document.getElementsByName('addpeak')[0];
+          frm.reset();
+          location.reload(); 
+        }
+      }
+    });
+  }
+  
+})
+
+
+}
 
 //autocomplete for driver city field
 driver_city_autocomplete = new google.maps.places.Autocomplete((document.getElementById('city')),
