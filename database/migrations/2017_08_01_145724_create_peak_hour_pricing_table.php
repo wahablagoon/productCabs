d52af6cdee059a9d96493c491677c56596de678a
praveenak <ak@bluelagoontechnologies.com>
2017-08-15 20:52:40 +0530

    change --praveen ak

--- database/migrations/2017_08_01_145724_create_peak_hour_pricing_table.php
+++ database/migrations/2017_08_01_145724_create_peak_hour_pricing_table.php
@@ -18,6 +18,7 @@ class CreatePeakHourPricingTable extends Migration
             $table->string('start_time', 255);
             $table->string('end_time', 255);
             $table->string('days', 255);
+            $table->string('category', 255);
             $table->string('type', 255);
             $table->string('amount', 255);
             $table->timestamps();
--- database/seeds/CategoryTableSeeder.php
+++ database/seeds/CategoryTableSeeder.php
@@ -12,9 +12,9 @@ class CategoryTableSeeder extends Seeder
     public function run()
     {
         DB::table('category')->insert([
-        	['category_name' => 'SUV','price_km' => '10','price_minute' => '10','max_size'=>'10','price_fare'=>'20','logo'=>'suv_default.png','marker'=>'marker.png'],
-        	['category_name' => 'Sedan','price_km' => '10','price_minute' => '10','max_size'=>'10','price_fare'=>'20','logo'=>'sedan_default.png','marker'=>'marker.png'],
-        	['category_name' => 'Hatchback','price_km' => '10','price_minute' => '10','max_size'=>'10','price_fare'=>'20','logo'=>'hatchback_default.png','marker'=>'marker.png']
+        	['category_name' => 'SUV','price_km' => '10','price_minute' => '10','max_size'=>'10','price_fare'=>'20','logo'=>'SUV-512.png','marker'=>'SUV-512.png'],
+        	['category_name' => 'Sedan','price_km' => '10','price_minute' => '10','max_size'=>'10','price_fare'=>'20','logo'=>'vehicle-icon-png-car-sedan-4.png','marker'=>'vehicle-icon-png-car-sedan-4.png'],
+        	['category_name' => 'Hatchback','price_km' => '10','price_minute' => '10','max_size'=>'10','price_fare'=>'20','logo'=>'hatchback-512.png','marker'=>'hatchback-512.png']
         ]);
     }
 }
